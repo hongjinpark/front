419b236f2c08266df857afaad4f5e7da4ae6634e
sunyoung <xymnl1212@gmail.com>
2023-11-20 18:04:31 +0900

    상품상세 페이지 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.module.css';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Test from './components/Test';
+// import Test from './components/Test';
 import Login from './pages/Login';
 import { AuthProvider } from './context/AuthProvider';
 import Nav from './components/Nav';
@@ -12,6 +12,7 @@ import Admin from './pages/Admin';
 import Lounge from './pages/Lounge';
 import NotFoundPage from './pages/NotFoundPage';
 import ProductLists from './pages/ProductLists';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -21,15 +22,15 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/linkPage" element={<LinkPage />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/lounge" element={<Lounge />} />
+            <Route path="login" element={<Login />} />
+            <Route path="linkPage" element={<LinkPage />} />
+            <Route path="admin" element={<Admin />} />
+            <Route path="lounge" element={<Lounge />} />
+            <Route path=":pdCategory" element={<ProductDetail />} />
+            <Route path="product" element={<ProductLists />} />
             <Route path="*" element={<NotFoundPage />} />
-            <Route path="/product" element={<ProductLists />} />
           </Route>
-
-          <Route path="/test" element={<Test />} />
+          {/* <Route path="/test" element={<Test />} /> */}
         </Routes>
         <Footer />
       </AuthProvider>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,26 @@
 import styles from './Products.module.css';
 import Card from '../components/ItemCard';
+import { Link } from 'react-router-dom';
 
 export default function Products({ list }) {
   return (
     <ul className={styles.products}>
       {list.map((item) => {
         return (
-          <li key={item.pdTitle}>
-            <Card className={styles.card}>
-              <div className={styles.info}>
-                <p>상품명 : {item.pdTitle}</p>
-                <p>가격 : {item.price}</p>
-              </div>
-            </Card>
+          <li key={item.id}>
+            <Link to={`${item.pdCategory}`}>
+              <Card className={styles.card}>
+                <img
+                  src={`data:image/jpeg;base64,${item.images[0].data}`}
+                  alt="상품이미지"
+                  className={styles.img}
+                />
+                <div className={styles.info}>
+                  <p>상품명 : {item.pdTitle}</p>
+                  <p>가격 : {item.price}</p>
+                </div>
+              </Card>
+            </Link>
           </li>
         );
       })}
