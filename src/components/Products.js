4e43cbcf8d4b120ff052a69950ff8b0a9308380e
sunyoung <xymnl1212@gmail.com>
2023-11-25 18:44:32 +0900

    상품 경로 변경

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,10 @@
 import styles from './Products.module.css';
 import Card from '../components/ItemCard';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Products({ list }) {
+  const { pdCategory } = useParams();
+  console.log('pdCategory:', pdCategory);
   if (!list || list.length === 0) {
     return <h1>원하시는 상품이 없습니다.</h1>;
   } else if (Array.isArray(list)) {
@@ -11,21 +13,39 @@ export default function Products({ list }) {
         {list.map((item) => {
           return (
             <li key={item.product_id}>
-              <Link to={`${item.pdTitle}`}>
-                <div>
-                  <Card className={styles.card}>
-                    <img
-                      src={require(`../assets${item.imgUrl}`)}
-                      alt="상품이미지"
-                      className={styles.img}
-                    />
-                    <div className={styles.info}>
-                      <p>상품명 : {item.pdTitle}</p>
-                      <p>가격 : {item.price}</p>
-                    </div>
-                  </Card>
-                </div>
-              </Link>
+              {pdCategory ? (
+                <Link to="/">
+                  <div>
+                    <Card className={styles.card}>
+                      <img
+                        src={require(`../assets${item.imgUrl}`)}
+                        alt="상품이미지"
+                        className={styles.img}
+                      />
+                      <div className={styles.info}>
+                        <p>상품명 : {item.pdTitle}</p>
+                        <p>가격 : {item.price}</p>
+                      </div>
+                    </Card>
+                  </div>
+                </Link>
+              ) : (
+                <Link to={`${item.pdTitle}`}>
+                  <div>
+                    <Card className={styles.card}>
+                      <img
+                        src={require(`../assets${item.imgUrl}`)}
+                        alt="상품이미지"
+                        className={styles.img}
+                      />
+                      <div className={styles.info}>
+                        <p>상품명 : {item.pdTitle}</p>
+                        <p>가격 : {item.price}</p>
+                      </div>
+                    </Card>
+                  </div>
+                </Link>
+              )}
             </li>
           );
         })}
@@ -35,21 +55,39 @@ export default function Products({ list }) {
     return (
       <ul className={styles.products}>
         <li key={list.product_id}>
-          <Link to={`${list.pdTitle}`}>
-            <div>
-              <Card className={styles.card}>
-                <img
-                  src={require(`../assets${list.imgUrl}`)}
-                  alt="상품이미지"
-                  className={styles.img}
-                />
-                <div className={styles.info}>
-                  <p>상품명 : {list.pdTitle}</p>
-                  <p>가격 : {list.price}</p>
-                </div>
-              </Card>
-            </div>
-          </Link>
+          {pdCategory ? (
+            <Link to={`../../${list.pdTitle}`}>
+              <div>
+                <Card className={styles.card}>
+                  <img
+                    src={require(`../assets${list.imgUrl}`)}
+                    alt="상품이미지"
+                    className={styles.img}
+                  />
+                  <div className={styles.info}>
+                    <p>상품명 : {list.pdTitle}</p>
+                    <p>가격 : {list.price}</p>
+                  </div>
+                </Card>
+              </div>
+            </Link>
+          ) : (
+            <Link to={`${list.pdTitle}`}>
+              <div>
+                <Card className={styles.card}>
+                  <img
+                    src={require(`../assets${list.imgUrl}`)}
+                    alt="상품이미지"
+                    className={styles.img}
+                  />
+                  <div className={styles.info}>
+                    <p>상품명 : {list.pdTitle}</p>
+                    <p>가격 : {list.price}</p>
+                  </div>
+                </Card>
+              </div>
+            </Link>
+          )}
         </li>
       </ul>
     );
