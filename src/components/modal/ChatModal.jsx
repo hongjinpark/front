35f3f834a3d75b705d51c0f5de1a3dc3ba34ac15
jongjin97 <kakao1302@naver.com>
2024-01-06 01:17:45 +0900

    채팅 모달 기본 틀

--- src/components/modal/ChatModal.jsx
+++ src/components/modal/ChatModal.jsx
@@ -1,14 +1,180 @@
+import { useEffect, useState } from 'react';
 import ChatModalContext from '../../context/ChatModalProvider';
 import Modal from './Modal';
 
 export default function ChatModal() {
+  const [chatRoom, setChatRoom] = useState();
+  useEffect(() => {
+    console.log(chatRoom);
+  }, []);
+
+  const backButton = () => {
+    return (
+      <button
+        onClick={() => setChatRoom()}
+        className="w-full h-full font-normal text-base text-[rgb(20,19,19)] flex justify-center items-center"
+        name="뒤로가기"
+      >
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-auto rotate-0"
+        >
+          <path
+            d="M12.5 19.5L4.42491 12.3749C4.19932 12.1759 4.19932 11.8241 4.42491 11.6251L12.5 4.5"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      </button>
+    );
+  };
+  const chatBody = () => {
+    return (
+      <>
+        <div className="h-full overflow-auto">
+          <div className="p-5 h-full bg-[#e9edef] overflow-auto">
+            <div></div>
+            <div>
+              <span className="block text-center text-[14px] py-4">
+                2024년 1월 5일
+              </span>
+              {/* 나 */}
+              <div>
+                <div type="textMessage">
+                  <div className="flex items-end w-auto mb-2 flex-start space-x-1 flex-row-reverse space-x-reverse">
+                    <div className="p-3 rounded-xl h-auto rounded-tr bg-[#363C45] text-white w-auto">
+                      <p className="break-all whitespace-pre-wrap">text1</p>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="block text-[13px] uppercase text-end">
+                        오후 5:56
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* 상대 */}
+              <div>
+                <div type="textMessage">
+                  <div className="flex items-end w-auto mb-2 flex-start space-x-1">
+                    <div className="p-3 rounded-xl h-auto rounded-tl bg-white w-auto">
+                      <p className="break-all whitespace-pre-wrap">text2</p>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="block text-[13px] uppercase text-start">
+                        오후 5:56
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#e9edef] py-4 border-t-[2px] border-t-slate-300 px-3">
+          {/* <form className="bg-[#F7F9FA] py-3 px-3 flex flex-col rounded-xl focus-within:shadow-banner h-auto"> */}
+          <textarea
+            title="채팅"
+            autoComplete="off"
+            maxLength="1000"
+            className="shrink-0 bg-transparent placeholder:text-[#9CA3AF] outline-none resize-none text-md h-16 w-full pre-wrap"
+            placeholder="메시지를 입력해주세요"
+            name="chat"
+          ></textarea>
+          <div className="flex justify-between mt-3">
+            <div className="flex">
+              <div>
+                <div className="block">
+                  <label
+                    className="text-gray-600 font-semibold text-sm leading-none cursor-pointer block border-[#e1e1e1] text-center border-0 m-0 p-0 w-6 h-6"
+                    htmlFor="chat-image-upload"
+                  >
+                    <img
+                      src="file-upload.png"
+                      name="image-upload"
+                      id="image-upload"
+                      alt="img"
+                    />
+                  </label>
+                  <input
+                    id="chat-image-upload"
+                    name="chat-image-upload"
+                    type="file"
+                    className="hidden px-4 md:px-5 appearance-none border text-input text-xs lg:text-sm font-body placeholder-body min-h-12 transition duration-200 ease-in-out bg-gray-100 border-gray-300 focus:shadow focus:bg-white focus:border-primary a11yHidden w-auto py-0 rounded-md"
+                    autoComplete="off"
+                    spellCheck="false"
+                    aria-invalid="false"
+                    accept="image/png, image/jpeg, image/jpg"
+                    multiple=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-end space-x-2">
+              <span className="text-sm leading-5 text-gray-400">0 / 1000</span>
+              <button type="submit" disabled="" className="w-6 h-6">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 512 512"
+                  className="w-full h-full fill-[#9CA3AF]"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M8 256C8 119 119 8 256 8s248 111 248 248-111 248-248 248S8 393 8 256zm143.6 28.9l72.4-75.5V392c0 13.3 10.7 24 24 24h16c13.3 0 24-10.7 24-24V209.4l72.4 75.5c9.3 9.7 24.8 9.9 34.3.4l10.9-11c9.4-9.4 9.4-24.6 0-33.9L273 107.7c-9.4-9.4-24.6-9.4-33.9 0L106.3 240.4c-9.4 9.4-9.4 24.6 0 33.9l10.9 11c9.6 9.5 25.1 9.3 34.4-.4z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          {/* </form> */}
+        </div>
+      </>
+    );
+  };
+  const header = () => {
+    return (
+      <div className="border-t-2 px-4 min-h-[70px] basis-[70px] flex justify-between items-center">
+        <a className="flex flex-grow h-10 gap-4" href="/product/143349249">
+          <div className="relative w-10 h-10 aspect-square">
+            <img
+              alt="상품 썸네일"
+              src="favicon.ico" // 상품 이미지
+              decoding="async"
+              data-nimg="fill"
+              className="rounded-md"
+              loading="lazy"
+              // style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"
+            />
+          </div>
+          <div>
+            <div className="flex items-center">
+              <span className="font-semibold text-[15px]">product_price</span>
+            </div>
+            <span className="block text-[12px]">product_Title</span>
+          </div>
+        </a>
+      </div>
+    );
+  };
   const body = () => {
     return (
       <div className="h-full">
         {/* chat room */}
         <ul className="flex flex-col h-full overflow-auto bg-white overscroll-contain">
           {/* 1번째 */}
-          <li className="flex justify-between px-4 gap-5 w-full cursor-pointer bg-white">
+          <li
+            className="flex justify-between px-4 gap-5 w-full cursor-pointer bg-white"
+            onClick={() => setChatRoom(1)}
+            role="presentation"
+          >
             <div className="flex py-3 border-t-[1px] border-gray-200 w-[90%]">
               <div className="border border-gray-100 relative flex flex-shrink-0 items-center justify-center bg-gray-300 rounded-full overflow-hidden">
                 <img
@@ -36,7 +202,11 @@ export default function ChatModal() {
           </li>
           <div></div>
           {/* 2번째 */}
-          <li className="flex justify-between px-4 gap-5 w-full cursor-pointer bg-white">
+          <li
+            className="flex justify-between px-4 gap-5 w-full cursor-pointer bg-white"
+            onClick={() => setChatRoom(2)}
+            role="presentation"
+          >
             <div className="flex py-3 border-t-[1px] border-gray-200 w-[90%]">
               <div className="border border-gray-100 relative flex flex-shrink-0 items-center justify-center bg-gray-300 rounded-full overflow-hidden">
                 <img
@@ -70,8 +240,10 @@ export default function ChatModal() {
   return (
     <Modal
       ModalContext={ChatModalContext}
-      title={'채팅'}
-      bodyContent={body()}
+      title={chatRoom ? 'userName' : '채팅'}
+      bodyContent={chatRoom ? chatBody() : body()}
+      headerContent={chatRoom ? header() : null}
+      backButton={backButton()}
     />
   );
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -7,6 +7,7 @@ export default function Modal({
   bodyContent,
   alterContent,
   popupContent,
+  backButton,
 }) {
   const { isOpen, closeModal } = useContext(ModalContext);
   const [modalVisible, setModalVisible] = useState(isOpen);
@@ -22,12 +23,6 @@ export default function Modal({
   if (!isOpen) {
     return null;
   }
-  const handleButtonClose = () => {
-    setModalVisible(false);
-    setTimeout(() => {
-      closeModal();
-    }, 300);
-  };
   const handleClose = (e) => {
     const modal = document.getElementById('modal');
     const target = e.target;
@@ -53,7 +48,7 @@ export default function Modal({
           className="relative  w-full md:w-[600px] h-full lg:h-auto z-50"
           id="modal"
         >
-          <div className="flex flex-col sm:h-screen sm:max-h-screen h-full max-h-full overflow-auto bg-white mx-auto my-0 pb-5">
+          <div className="flex flex-col sm:h-screen sm:max-h-screen h-full max-h-full overflow-auto bg-white mx-auto my-0">
             <header className="sticky z-[99] top-0">
               <div className="static">
                 <div className="relative flex justify-between items-center w-full h-11 z-[9] px-5 py-[11px]">
@@ -61,35 +56,14 @@ export default function Modal({
                     {title}
                   </h1>
                   <div className="w-9 h-8 flex justify-center items-center text-left z-[1]">
-                    <button
-                      onClick={() => handleButtonClose()}
-                      className="w-full h-full font-normal text-base text-[rgb(20,19,19)] flex justify-center items-center"
-                      name="뒤로가기"
-                    >
-                      <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-auto rotate-0"
-                      >
-                        <path
-                          d="M12.5 19.5L4.42491 12.3749C4.19932 12.1759 4.19932 11.8241 4.42491 11.6251L12.5 4.5"
-                          stroke="currentColor"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>
-                      </svg>
-                    </button>
+                    {backButton}
                   </div>
                 </div>
               </div>
             </header>
             <div className="flex flex-col flex-1 w-full h-full overflow-y-hidden pt-0">
               {headerContent}
-              <div>{bodyContent}</div>
+              {bodyContent}
             </div>
           </div>
         </div>
